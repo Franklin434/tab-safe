3b90e25fbc85865f0e507d6545f8a503ccda4488
Franklin434 <ukachukwufranklin434@gmail.com>
2025-12-22 19:39:41 +0100

    added input saving functionality

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 let myLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
-
+const ulEl = document.getElementById("ul-el")
 
 
 inputBtn.addEventListener("click",  function(){
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
+    if (inputEl.value !== "") {
+        myLeads.push(inputEl.value)
+        renderLead()
+        inputEl.value = ""
+    } else {
+        alert("input a valid url")
+    }
+    
 }) 
 
+function renderLead(){
+    let listItem = `
+    <li>
+        <a href="${inputEl.value}" target = "_blank"> ${inputEl.value}</a>
+    </li>`
+    ulEl.innerHTML += listItem
+}
+// console.log()
+// function renderLeads(){
+//     let listItems = ""
+//     for (i = 0; i < myLeads.length; i++){
+//         listItems += `<li><a href="${myLeads[i]}" target ="_blank"> ${myLeads[i]} </a></li>`
+//     }
+//     ulEl.innerHTML = listItems
+// }
