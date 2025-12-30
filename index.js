d324c937ad6b4157235fb4151222afeeae43bc83
Franklin434 <ukachukwufranklin434@gmail.com>
2025-12-30 06:31:09 +0100

    added core functionality of the extension

--- index.js
+++ index.js
@@ -2,38 +2,62 @@ let myLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage =JSON.parse( localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+        console.log(tabs)
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+function render(leads){
+    let listItems = ""
+    for (i = 0; i < leads.length; i++){
+        listItems += `<li><a href="${leads[i]}" target ="_blank"> ${leads[i]} </a></li>`
+    }
+    ulEl.innerHTML = listItems
+}
 
 inputBtn.addEventListener("click",  function(){
     if (inputEl.value !== "") {
         myLeads.push(inputEl.value)
-        renderLead()
         inputEl.value = ""
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+        render(myLeads)
+
+        // console.log( localStorage.getItem("myLeads"))
     } else {
         alert("input a valid url")
     }
     
 }) 
 
-function renderLead(){
-    let listItem = `
-    <li>
-        <a href="${inputEl.value}" target = "_blank"> ${inputEl.value}</a>
-    </li>`
-    ulEl.innerHTML += listItem
-}
-// console.log()
-// function renderLeads(){
-//     let listItems = ""
-//     for (i = 0; i < myLeads.length; i++){
-//         listItems += `<li><a href="${myLeads[i]}" target ="_blank"> ${myLeads[i]} </a></li>`
-//     }
-//     ulEl.innerHTML = listItems
+// function renderLead(){
+//     let listItem = `
+//     <li>
+//         <a href="${inputEl.value}" target = "_blank"> ${inputEl.value}</a>
+//     </li>`
+//     ulEl.innerHTML += listItem
 // }
+// console.log()
 
 
-const recepient = "josh"
-const sender = "frank"
-const email = `hey ${recepient}! How is it going? Cheers ${sender}`
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
-console.log(email)
